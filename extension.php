320e90c19844ada7ec1ac38e8952591815c5e460
Magnus Kokk <magnus@kokk.eu>
2023-02-15 00:12:48 +0200

    Use custom query for 1s precision, explicitly check for min/default TTL also

--- extension.php
+++ extension.php
@@ -1,24 +1,20 @@
 <?php
 
+require_once(__DIR__ . "/stats.php");
+
 class AutoTTLExtension extends Minz_Extension
 {
     // Defaults
     private const MAX_TTL = 24 * 60 * 60; // 1 day
 
     /**
-     * @var FreshRSS_StatsDAO
+     * @var AutoTTLStats
      */
-    private $statsDAO = null;
-
-    /**
-     * @var FreshRSS_FeedDAO
-     */
-    private $feedDAO = null;
+    private $stats;
 
     public function init()
     {
-        $this->statsDAO = FreshRSS_Factory::createStatsDAO();
-        $this->feedDAO = FreshRSS_Factory::createFeedDao();
+        $this->stats = new AutoTTLStats();
         $this->registerHook('feed_before_actualize', array($this, 'feedBeforeActualizeHook'));
 
         if (is_null(FreshRSS_Context::$user_conf->auto_ttl_max_ttl)) {
@@ -44,58 +40,47 @@ class AutoTTLExtension extends Minz_Extension
         }
 
         $now = time();
+        $minTTL = $this->getMinTTL($feed);
         $maxTTL = (int)FreshRSS_Context::$user_conf->auto_ttl_max_ttl;
-        $count = $this->feedDAO->countEntries($feed->id());
+        $d = $now - $feed->lastUpdate();
 
-        if ($count < 2) {
-            if ($now - $feed->lastUpdate() > $maxTTL) {
-                return $feed;
-            }
-
-            $this->debug("unable to calculate avg, falling back to maxTTL", $feed, $count, $maxTTL);
-
-            return null;
-        }
-
-        // Calculate average seconds between feed entries.
-        $perHour = $this->statsDAO->calculateEntryAveragePerFeedPerHour($feed->id());
-
-        if ((int)$perHour === (int)$count) {
-            // All entries have the same date. May happen with XPath scraping when date selector is not specified.
-            if ($now - $feed->lastUpdate() > $maxTTL) {
-                return $feed;
-            }
-
-            $this->debug("all entries have the same date, falling back to maxTTL", $feed, $count, $maxTTL);
-
-            return null;
-        }
-
-        $ttl = (int)((1 / $perHour) * 60 * 60);
-        if ($ttl > $maxTTL) {
-            $ttl = $maxTTL;
-        }
-
-        if ($now - $feed->lastUpdate() > $ttl) {
+        if ($d > $maxTTL) {
             return $feed;
         }
 
-        $this->debug("avg TTL not exceeded yet", $feed, $count, $ttl);
+        $avgTTL = $this->stats->calcAvgTTL($feed->id());
 
-        return null;
+        if ($avgTTL === 0) {
+            $this->debug($feed, sprintf('unable to calculate avgTTL, falling back to maxTTL (%ds)', $maxTTL));
+            return null;
+        }
+
+        if ($d < $minTTL) {
+            $this->debug($feed, sprintf('minTTL (%ds) not exceeded yet', $minTTL));
+            return null;
+        }
+
+        if ($d < $avgTTL) {
+            $this->debug($feed, sprintf('avgTTL (%ds) not exceeded yet', $avgTTL));
+            return null;
+        }
+
+        return $feed;
     }
 
-    private function debug(string $msg, FreshRSS_Feed $feed, int $count, int $ttl)
+    private function getMinTTL(FreshRSS_Feed $feed): int
+    {
+        $ttl = $feed->ttl();
+        return $ttl == FreshRSS_Feed::TTL_DEFAULT ? FreshRSS_Context::$user_conf->ttl_default : $ttl;
+    }
+
+    private function debug(FreshRSS_Feed $feed, string $msg)
     {
         Minz_Log::debug(sprintf(
-            'AutoTTL: skip feed %d (%s), %s, count: %d, TTL: %ds, last update at %s, next update at %s',
+            'AutoTTL: skip feed %d (%s): %s',
             $feed->id(),
             $feed->name(),
             $msg,
-            $count,
-            $ttl,
-            date('Y-m-d H:i:s', $feed->lastUpdate()),
-            date('Y-m-d H:i:s', $feed->lastUpdate() + $ttl),
         ));
     }
 }
---
+++ stats.php
@@ -0,0 +1,28 @@
+<?php
+
+class AutoTTLStats extends Minz_ModelPdo
+{
+    /**
+     * Calculates the average seconds between articles.
+     * Returns 0 when not enough articles or all articles have the same timestamp.
+     */
+    public function calcAvgTTL(int $feed): int
+    {
+        $sql = <<<SQL
+SELECT COUNT(1) AS count
+, MIN(date) AS date_min
+, MAX(date) AS date_max
+FROM `_entry` AS e
+WHERE e.id_feed = {$feed}
+SQL;
+        $stm = $this->pdo->query($sql);
+        $res = $stm->fetch(PDO::FETCH_NAMED);
+
+        if ($res['count'] > 0) {
+            $interval_in_seconds = (int)$res['date_max'] - (int)$res['date_min'];
+            return (int)($interval_in_seconds / (int)$res['count']);
+        }
+
+        return 0;
+    }
+}
