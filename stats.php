f8dd9ea6b62e1c26a50a7acae5ef33dd79fad8b1
Magnus Kokk <magnus@kokk.eu>
2024-03-09 01:45:20 +0200

    Implement informative statistics table

--- stats.php
+++ stats.php
@@ -2,7 +2,7 @@
 
 class AutoTTLStats extends Minz_ModelPdo
 {
-    private function calcAdjustedTTL(int $avgTTL, int $dateMax): int
+    public function calcAdjustedTTL(int $avgTTL, int $dateMax): int
     {
         $defaultTTL = FreshRSS_Context::$user_conf->ttl_default;
         $maxTTL = (int) FreshRSS_Context::$user_conf->auto_ttl_max_ttl;
@@ -45,8 +45,7 @@ SQL;
         );
     }
 
-    // fetchAllStats returns stats for AutoTTL enabled feeds.
-    public function fetchAllStats(): array
+    public function getAutoTTLFeeds(): array
     {
         $limit = FreshRSS_Context::$user_conf->auto_ttl_stats_count;
 
@@ -73,48 +72,84 @@ LIMIT {$limit}
 SQL;
         $stm = $this->pdo->query($sql);
         $res = $stm->fetchAll(PDO::FETCH_NAMED);
-        $now = new \DateTime();
-        $now->setTimezone(new DateTimeZone(date_default_timezone_get()));
-
-        foreach ($res as $i => $feedStat) {
-            $adjustedTTL = $this->calcAdjustedTTL(
-                (int) $feedStat['avgTTL'],
-                (int) $feedStat['date_max'],
-            );
-            $res[$i]['adjustedTTL'] = $adjustedTTL;
-
-            $nextUpdate = DateTime::createFromFormat('U', (int) $feedStat['lastUpdate'] + $adjustedTTL);
-            $nextUpdate->setTimezone(new DateTimeZone(date_default_timezone_get()));
-            $res[$i]['nextUpdateAfter'] = human_interval($nextUpdate->diff($now));
-        }
 
         return $res;
     }
-}
 
-function human_interval(\DateInterval $interval): string
-{
-    $results = [];
+    public function getNonAutoTTLFeeds(): array
+    {
+        $limit = FreshRSS_Context::$user_conf->auto_ttl_stats_count;
 
-    if ($interval->y > 0) {
-        $results[] = "{$interval->y} years";
+        $sql = <<<SQL
+SELECT
+	feed.id,
+	feed.name,
+	feed.`lastUpdate`,
+	feed.ttl,
+	CASE WHEN stats.count > 0 THEN ((stats.date_max - stats.date_min) / stats.count) ELSE 0 END AS `avgTTL`
+FROM (
+	SELECT
+		id_feed,
+		COUNT(1) AS count,
+		MIN(date) AS date_min,
+		MAX(date) AS date_max
+	FROM `_entry`
+	GROUP BY id_feed
+) AS stats
+LEFT JOIN `_feed` as feed ON feed.id = stats.id_feed
+WHERE feed.ttl != 0
+ORDER BY `avgTTL` ASC
+LIMIT {$limit}
+SQL;
+        $stm = $this->pdo->query($sql);
+        $res = $stm->fetchAll(PDO::FETCH_NAMED);
+
+        return $res;
     }
 
-    if ($interval->m > 0) {
-        $results[] = "{$interval->m} months";
+    public function humanInterval(\DateInterval $interval): string
+    {
+        $results = [];
+
+        if ($interval->y === 1) {
+            $results[] = "{$interval->y} year";
+        } elseif ($interval->y > 1) {
+            $results[] = "{$interval->y} years";
+        }
+
+        if ($interval->m === 1) {
+            $results[] = "{$interval->m} month";
+        } elseif ($interval->m > 1) {
+            $results[] = "{$interval->m} months";
+        }
+
+        if ($interval->d === 1) {
+            $results[] = "{$interval->d} day";
+        } elseif ($interval->d > 1) {
+            $results[] = "{$interval->d} days";
+        }
+
+        if ($interval->h === 1) {
+            $results[] = "{$interval->h} hour";
+        } elseif ($interval->h > 1) {
+            $results[] = "{$interval->h} hours";
+        }
+
+        if ($interval->i === 1) {
+            $results[] = "{$interval->i} minute";
+        } elseif ($interval->i > 1) {
+            $results[] = "{$interval->i} minutes";
+        }
+
+        return implode(' ', $results);
     }
 
-    if ($interval->d > 0) {
-        $results[] = "{$interval->d} days";
-    }
+    public function humanIntervalFromSeconds(int $seconds): string
+    {
+        $from = new \DateTime('@0');
+        $to = new \DateTime("@$seconds");
+        $interval = $from->diff($to);
 
-    if ($interval->h > 0) {
-        $results[] = "{$interval->h} hours";
+        return $this->humanInterval($interval);
     }
-
-    if ($interval->i > 0) {
-        $results[] = "{$interval->i} minutes";
-    }
-
-    return implode(' ', $results);
 }
