d42af24fb614c15b4e14d20af34da51b79766a72
Magnus Kokk <magnus@kokk.eu>
2026-07-30 21:50:37 +0300

    Run tests for all databases

--- test.sh
+++ test.sh
@@ -1,9 +1,14 @@
 #!/bin/env bash
 
+set -eu
+
 USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 export USER_ID
 export GROUP_ID
+
+docker compose run --rm -it composer install
+docker compose run --rm -it composer dump-autoload
 
 function test_sqlite {
 	trap "docker compose down" RETURN
@@ -24,19 +29,64 @@ function test_sqlite {
 		./cli/create-user.php \
 		--user admin
 
-	docker compose run --rm -it composer install
-	docker compose run --rm -it composer dump-autoload
+	docker compose exec -w /var/www/FreshRSS/extensions/FreshRSS-AutoTTL freshrss \
+		./vendor/bin/phpunit tests
+}
+
+function test_mysql {
+	trap "docker compose down" RETURN
+
+	docker compose up -d mysql wiremock freshrss
+
+	docker compose exec -w /var/www/FreshRSS freshrss \
+		./cli/prepare.php
+
+	docker compose exec -w /var/www/FreshRSS freshrss \
+		./cli/do-install.php \
+		--default-user admin \
+		--auth-type none \
+		-- environment development \
+		--db-type mysql \
+		--db-host mysql \
+		--db-user freshrss \
+		--db-password freshrss \
+		--db-base freshrss
+
+	docker compose exec -w /var/www/FreshRSS freshrss \
+		./cli/create-user.php \
+		--user admin
+
+	docker compose exec -w /var/www/FreshRSS/extensions/FreshRSS-AutoTTL freshrss \
+		./vendor/bin/phpunit tests
+}
+
+function test_postgres {
+	trap "docker compose down" RETURN
+
+	docker compose up -d postgres wiremock freshrss
+
+	docker compose exec -w /var/www/FreshRSS freshrss \
+		./cli/prepare.php
+
+	docker compose exec -w /var/www/FreshRSS freshrss \
+		./cli/do-install.php \
+		--default-user admin \
+		--auth-type none \
+		-- environment development \
+		--db-type pgsql \
+		--db-host postgres \
+		--db-user freshrss \
+		--db-password freshrss \
+		--db-base freshrss
+
+	docker compose exec -w /var/www/FreshRSS freshrss \
+		./cli/create-user.php \
+		--user admin
 
 	docker compose exec -w /var/www/FreshRSS/extensions/FreshRSS-AutoTTL freshrss \
 		./vendor/bin/phpunit tests
 }
 
 test_sqlite
-
-# --db-type can be: 'sqlite' (default), 'mysql' (MySQL or MariaDB), 'pgsql' (PostgreSQL).
-# --db-host URL of the database server. Default is 'localhost'.
-# --db-user sets database user.
-# --db-API password sets database password.
-# --db-base sets database name.
-# --db-prefix is an optional prefix in front of the names of the tables. We suggest using 'freshrss_' (default).
-# This command does not create the default user. Do that with ./cli/create-user.php.
+test_mysql
+test_postgres
